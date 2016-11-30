d2c627b5344bd0ef39bb84a24b99a6a480947e21
rafa <rafael.cabacas@gmail.com>
2016-11-30 12:42:09 +0100

    vistas

--- cedo_preinscripcions/view/cursos/detalle_curso.php
+++ cedo_preinscripcions/view/cursos/detalle_curso.php
@@ -18,127 +18,132 @@
 		?>
 		<section id="content">
 			<h2>Detall Curs</h2>
-			<div style="margin-left: 150px; id="detall">			
-			<form method="post" id="formulario">
-				<label>Codi:</label>
-				<input type='text' name='codi' required="required" value="<?php echo $curso->codi;?>"/>
-				<label>Nom Curs:</label>
-			 	<input type='text' name='nom' readonly="readonly" required="required"  value="<?php echo $curso->nom;?>"/><br>				
-				<label>Area Formativa:</label>
-				<select  name="id_area" required="required">
-				<?php
-				foreach($areas as $c) {
-					echo '<option value="'.$c->id.'"';
-					if($curso->id_area == $c->id)echo(" selected='selected' ");
-					echo '>'.$c->nom.'</option>';
-				}
-				?>
-				</select>  
-			 	<label>Tipus:</label>
-			 	<input type='text' name='tipus' readonly="readonly" value="<?php echo $curso->tipus;?>"/><br>
-			 	<label>Hores:</label>
-			 	<input type='number' name='hores' readonly="readonly" min="1" max="100000"  value="<?php echo $curso->hores;?>"/>
-			 	<label>Data d'Inici:</label>
-			 	<input type='date' name='data_inici' readonly="readonly" value="<?php echo $curso->data_inici;?>"/><br>
-			 	<label>Data d'Fi:</label>
-			 	<input type='date' name='data_fi' readonly="readonly" value="<?php echo $curso->data_fi;?>"/>
-			 	<label>Horari:</label>
-			 	<input type='text' name='horari' readonly="readonly" value="<?php echo $curso->horari;?>"/><br>
-			 	<label>Torn:</label>
-				<select  name='torn'  readonly="readonly">
-				  <option value="M" <?php if($curso->torn == 'M'){echo("selected");}?>>Mañana</option>
-				  <option value="T" <?php if($curso->torn == 'T'){echo("selected");}?>>Tarde</option>
-				  <option value="N" <?php if($curso->torn == 'N'){echo("selected");}?>>Nocturno</option>
-				</select><br>
-			 	<label>Requisits:</label>
-			 	<input type='text' readonly="readonly" name='requisits'  value="<?php echo $curso->requisits;?>"/><br>
-			 	<label>Descripció:</label>
-				<textarea rows="12" cols="60" readonly="readonly" name='descripcio'> <?php echo $curso->descripcio;?></textarea>
-				 	
-		 </form>
-			<?php				
-				if($usuario){
-					if(!$usuario->admin){
-					//echo '<p><b><a href="index.php?controlador=preinscripcion&operacion=guardar&parametro='.$curso->id.'" >Preinscrivirse</a></b></p>';
-					
-					echo '<p><b><a href="index.php?controlador=preinscripcion&operacion=guardar&parametro='
-							.$curso->id.'">';
-							
-							echo "";
-							echo "<img class='boton' src='images/buttons/guardar.png' alt='guardar' title='Preinscribir-se curs'
-									<span>Preinscriure's</span>";							
-					echo '</a></b>';
-					
-					echo '<b><a href="index.php?controlador=subscripcion&operacion=guardar&parametro='.$curso->id_area.'" >';
-						echo "<img class='boton' src='images/buttons/listado.png' alt='Subscripció àrea' title='Subscripció àrea'
-								style='margin-left:100px;/>";	
-						
-					echo "</a></b></p>";
-							
-							
-					//echo '<p><b><a href="index.php?controlador=subscripcion&operacion=guardar&parametro='.$curso->id_area.'" >Suscrite al Area Formativa</a></b></p>';
+			<div style="margin-left: 60px; id="detall">			
+				<form method="post" id="formulario">
+					<label>Codi:</label>
+					<input type='text' name='codi' required="required" value="<?php echo $curso->codi;?>"/>
+					<label>Nom Curs:</label>
+				 	<input type='text' name='nom' readonly="readonly" required="required"  value="<?php echo $curso->nom;?>"/><br>				
+					<label>Area Formativa:</label>
+					<select  name="id_area" required="required">
+					<?php
+					foreach($areas as $c) {
+						echo '<option value="'.$c->id.'"';
+						if($curso->id_area == $c->id)echo(" selected='selected' ");
+						echo '>'.$c->nom.'</option>';
 					}
-					//Si es administrador
-					if($usuario->admin){				
-						//echo '<p><b><a href="index.php?controlador=Curso&operacion=modificar&parametro='
-						//	.$curso->id.'" >Modificar Curs</a></b></p>';
-						echo '<p><b><a href="index.php?controlador=Curso&operacion=modificar&parametro='
-							.$curso->id.'">';
-						echo "<img class='boton' src='images/buttons/editar.png' alt='detalls' title='Modificar curs'/>";
-						echo '</a></b>';	
+					?>
+					</select>  
+				 	<label>Tipus:</label>
+				 	<input type='text' name='tipus' readonly="readonly" value="<?php echo $curso->tipus;?>"/><br>
+				 	<label>Hores:</label>
+				 	<input type='number' name='hores' readonly="readonly" min="1" max="100000"  value="<?php echo $curso->hores;?>"/>
+				 	<label>Data d'Inici:</label>
+				 	<input type='date' name='data_inici' readonly="readonly" value="<?php echo $curso->data_inici;?>"/><br>
+				 	<label>Data d'Fi:</label>
+				 	<input type='date' name='data_fi' readonly="readonly" value="<?php echo $curso->data_fi;?>"/>
+				 	<label>Horari:</label>
+				 	<input type='text' name='horari' readonly="readonly" value="<?php echo $curso->horari;?>"/><br>
+				 	<label>Torn:</label>
+					<select  name='torn'  readonly="readonly">
+					  <option value="M" <?php if($curso->torn == 'M'){echo("selected");}?>>Mañana</option>
+					  <option value="T" <?php if($curso->torn == 'T'){echo("selected");}?>>Tarde</option>
+					  <option value="N" <?php if($curso->torn == 'N'){echo("selected");}?>>Nocturno</option>
+					</select><br>
+				 	<label>Requisits:</label>
+				 	<input type='text' readonly="readonly" name='requisits'  value="<?php echo $curso->requisits;?>"/><br>
+				 	<label>Descripció:</label>
+					<textarea rows="12" cols="60" readonly="readonly" name='descripcio'> <?php echo $curso->descripcio;?></textarea><br>
+					 	
+			 </form>
+				<?php				
+					if($usuario){
+						if(!$usuario->admin){
+						//echo '<p><b><a href="index.php?controlador=preinscripcion&operacion=guardar&parametro='.$curso->id.'" >Preinscrivirse</a></b></p>';
 						
-						echo '<b><a href="index.php?controlador=Curso&operacion=baja&parametro='
+						echo '<br><p<b><a href="index.php?controlador=preinscripcion&operacion=guardar&parametro='
 								.$curso->id.'">';
-						echo "<img class='boton' src='images/buttons/borrar.png' 
-								style='margin-left: 20px; alt='elimiar' title='eliminar curs'/>";
+								
+								echo "";
+								echo "<img class='boton' src='images/buttons/guardar.png' alt='guardar' title='Preinscribir-se curs'
+										<span>Preinscriure's</span>";							
 						echo '</a></b>';
 						
-						echo"<input type='buttton' class='botonimprimir'
-						value='Imprimir' name='imprimir'onclick='window.print();'/>";
-						
-						echo '<b><a href="index.php?controlador=Preinscripcion&operacion=exportXML&parametro='.$curso->id.'" >';
-						echo "<img style='margin-bottom: 0px;
-											  height: 50px;
-											  width: 50px;
-											  padding-left: 0px;
-							                  border-left-width: 20px;
-							                  border-left-style: solid;
-											  margin-bottom:-5px;'
-								 class='botonexportxml' src='images/buttons/exportxml.png' alt='export XML' title='Exportació XML'/>";
+						echo '<b><a href="index.php?controlador=subscripcion&operacion=guardar&parametro='.$curso->id_area.'" >';
+							echo "<img class='boton' src='images/buttons/listado.png' alt='Subscripció àrea' title='Subscripció àrea'
+									style='margin-left:100px;/>";	
+							
 						echo "</a></b></p>";
-						//echo '<b><a href="index.php?controlador=Preinscripcion&operacion=exportXML&parametro='.$curso->id.'" >Exportar Preinscripcions a XML</a></b>';						
-					?>
-						</div>
-					<?php 
-					}
-					if($usuario || $usuario->admin){
-						echo '<h2>Alumnes Preinscrits al Curs<h2>'; 
-						echo '<table border=0.2 id="list_detalle">';
-						echo '<tr>';
-						echo '<th>DNI</th><th>NOM</th><th>TELF. MOBIL</th><th>TELF. FIXE</th><th>EMAIL</th><th>DATA</th>
-						<th>NOM CURS</th>'; 
-						if($usuario->admin) echo '<th class="accio">ACCIÓ</th>';
-						echo '<tr>';	
-						foreach($preinscripcions as $preinscripcio){
-							echo '<tr>';
-							echo "<td> $preinscripcio->dni </td>";
-							echo "<td> $preinscripcio->nom </td>";
-							echo "<td> $preinscripcio->telefon_mobil </td>";
-							echo "<td> $preinscripcio->telefon_fix</td>";
-							echo "<td> $preinscripcio->email</td>";
-							echo "<td> $preinscripcio->data</td>";
-							echo "<td> $preinscripcio->nom_curs</td>";
-							if($usuario->admin)
-							  echo '<td><b><a href="index.php?controlador=Preinscripcion&operacion=baja&parametro='
-								.$preinscripcio->id_curs.'&pu='.$preinscripcio->id_usuari.'" >Baixa</a></b></td>';
-							echo '</tr>';
+								
+								
+						//echo '<p><b><a href="index.php?controlador=subscripcion&operacion=guardar&parametro='.$curso->id_area.'" >Suscrite al Area Formativa</a></b></p>';
 						}
-						echo '</table><br>';
-					}
-					
-				}	
-				echo '';				
-		?>
+						//Si es administrador
+						if($usuario->admin){				
+							//echo '<p><b><a href="index.php?controlador=Curso&operacion=modificar&parametro='
+							//	.$curso->id.'" >Modificar Curs</a></b></p>';
+							echo '<p><b><a href="index.php?controlador=Curso&operacion=modificar&parametro='
+								.$curso->id.'">';
+							echo "<img class='boton' src='images/buttons/editar.png' alt='detalls' title='Modificar curs'/>";
+							echo '</a></b>';	
+							
+							echo '<b><a href="index.php?controlador=Curso&operacion=baja&parametro='
+									.$curso->id.'">';							
+							echo "<img class='boton' src='images/buttons/borrar.png' 
+									style='margin-left: 145px;				 							
+				 					alt='elimiar' title='eliminar curs'/>";
+							echo '</a></b>';
+							
+							echo"<input type='buttton' class='botonimprimir'
+				 					title='imprimir';				 					
+				 					style='margin-left: 145px;
+									value='Imprimir' name='imprimir'
+				 					onclick='window.print();'/>";
+							
+							echo '<b><a href="index.php?controlador=Preinscripcion&operacion=exportXML&parametro='.$curso->id.'" >';
+							echo "<img style='margin-bottom: 0px;
+												  height: 50px;
+												  width: 50px;
+												  padding-left: 0px;
+					  							  margin-left:145px;
+								                  border-left-width: 20px;
+								                  border-left-style: solid;
+												  margin-bottom:-5px;'
+									 class='botonexportxml' src='images/buttons/exportxml.png' alt='export XML' title='Exportació XML'/>";
+							echo "</a></b></p>";
+							//echo '<b><a href="index.php?controlador=Preinscripcion&operacion=exportXML&parametro='.$curso->id.'" >Exportar Preinscripcions a XML</a></b>';						
+						?>
+			</div>
+					<?php 
+						}
+						if($usuario || $usuario->admin){
+							echo '<h2>Alumnes Preinscrits al Curs<h2>'; 
+							echo '<table border=0.2 id="list_detalle">';
+							echo '<tr>';
+							echo '<th>DNI</th><th>NOM</th><th>TELF. MOBIL</th><th>TELF. FIXE</th><th>EMAIL</th><th>DATA</th>
+							<th>NOM CURS</th>'; 
+							if($usuario->admin) echo '<th class="accio">ACCIÓ</th>';
+							echo '<tr>';	
+							foreach($preinscripcions as $preinscripcio){
+								echo '<tr>';
+								echo "<td> $preinscripcio->dni </td>";
+								echo "<td> $preinscripcio->nom </td>";
+								echo "<td> $preinscripcio->telefon_mobil </td>";
+								echo "<td> $preinscripcio->telefon_fix</td>";
+								echo "<td> $preinscripcio->email</td>";
+								echo "<td> $preinscripcio->data</td>";
+								echo "<td> $preinscripcio->nom_curs</td>";
+								if($usuario->admin)
+								  echo '<td><b><a href="index.php?controlador=Preinscripcion&operacion=baja&parametro='
+									.$preinscripcio->id_curs.'&pu='.$preinscripcio->id_usuari.'" >Baixa</a></b></td>';
+								echo '</tr>';
+							}
+							echo '</table><br>';
+						}
+						
+					}	
+					echo '';				
+				?>
 
 		</section>
 		
